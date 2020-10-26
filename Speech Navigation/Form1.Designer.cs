5c2d82c76f22d2a1412129d40d1c65e04dba3c86
unknown <trajath66@gmail.com>
2020-10-26 12:44:48 +0530

    My First Commit Message

---
+++ Speech Navigation/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Speech_Navigation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startbtn = new System.Windows.Forms.Button();
+            this.stopbtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // startbtn
+            // 
+            this.startbtn.Location = new System.Drawing.Point(93, 2);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(75, 23);
+            this.startbtn.TabIndex = 0;
+            this.startbtn.Text = "Start";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click);
+            // 
+            // stopbtn
+            // 
+            this.stopbtn.Location = new System.Drawing.Point(598, 12);
+            this.stopbtn.Name = "stopbtn";
+            this.stopbtn.Size = new System.Drawing.Size(75, 23);
+            this.stopbtn.TabIndex = 1;
+            this.stopbtn.Text = "Stop";
+            this.stopbtn.UseVisualStyleBackColor = true;
+            this.stopbtn.Click += new System.EventHandler(this.stopbtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 49);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(809, 290);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 400);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.stopbtn);
+            this.Controls.Add(this.startbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.Button stopbtn;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Speech Navigation/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Speech_Navigation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechSynthesizer speechsynth = new SpeechSynthesizer();
+        SpeechRecognitionEngine receng = new SpeechRecognitionEngine();
+        Choices choice = new Choices();
+
+        private void startbtn_Click(object sender, EventArgs e)
+        {
+            startbtn.Enabled = false;
+            stopbtn.Enabled = true;
+            choice.Add(new string[] { "hello", "are you good", "what is the current time", "open blog", "thank you", "close" });
+            Grammar gr = new Grammar(new GrammarBuilder(choice));
+
+            try
+            {
+                receng.RequestRecognizerUpdate();
+                receng.LoadGrammar(gr);
+                receng.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(receng_SpeechRecognized);
+                receng.SetInputToDefaultAudioDevice();
+                receng.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+            void receng_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text.ToString())
+            {
+                case "hello":
+                    speechsynth.SpeakAsync("hai user");
+                    break;
+                case "are you good":
+                    speechsynth.SpeakAsync("iam fine. what about you");
+                    break;
+                case "what is the current time":
+                    speechsynth.SpeakAsync("right now it is " + DateTime.Now.ToLongTimeString());
+                    break;
+                case "thank you":
+                    speechsynth.SpeakAsync("well. same to you");
+                    break;
+                case "open google":
+                    Process.Start("chrome", "http://www.Google.com");
+                    break;
+                case "close":
+                    speechsynth.Speak("see you later have a good day. bye bye");
+                    Application.Exit();
+                    break;
+
+            }
+            listBox1.Items.Add(e.Result.Text.ToString());
+        }
+
+        private void stopbtn_Click(object sender, EventArgs e)
+        {
+            receng.RecognizeAsyncStop();
+            startbtn.Enabled = true;
+            stopbtn.Enabled = false;
+        }
+    }
+}
